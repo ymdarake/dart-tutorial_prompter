2ef87a59bc288512b40c333be89596d67797dce4
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-04 12:36:13 +1300

    [add]379: Example File

--- example/main.dart
+++ example/main.dart
@@ -7,5 +7,11 @@ void main() {
   ];
 
   final prompter = Prompter();
-  print(prompter.askBinary('Do you like dart'));
+
+  String colorCode = prompter.askMultiple('Select a color', options);
+
+  bool answer = prompter.askBinary('Do you like this lib?');
+
+  print(colorCode);
+  print(answer);
 }
