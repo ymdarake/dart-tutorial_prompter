eaa49543af4196d04537c374794895c78c41f936
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-04 11:39:23 +1300

    [add]362: Prompter Interface

--- example/main.dart
+++ example/main.dart
@@ -1,14 +1,12 @@
-import 'package:prompter_ymdarake/src/terminal.dart';
+import 'package:prompter_ymdarake/src/prompter.dart';
 import 'package:prompter_ymdarake/src/option.dart';
 
 void main() {
-  var terminal = Terminal();
   var options = [
     Option('I want red', '#f00'),
     Option('I want blue', '#00f'),
   ];
 
-  terminal.printOptions(options);
-  var response = terminal.collectInput();
-  print(response);
+  var prompter = Prompter();
+  prompter.ask('What color do you want', options);
 }
--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -1,1 +1,11 @@
-class Prompter {}
+import 'option.dart';
+import 'terminal.dart';
+
+class Prompter {
+  ask(String prompt, List<Option> options) {
+    var terminal = Terminal();
+
+    terminal.printPrompt(prompt);
+    terminal.printOptions(options);
+  }
+}
