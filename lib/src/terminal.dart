f27df93195d83870fc6caf0ffd783f1ef3c0e08e
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-04 11:26:12 +1300

    [init]terminal, option

---
+++ lib/src/terminal.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'option.dart';
+
+class Terminal {
+  void printPrompt(String prompt) {
+    stdout.writeln(prompt);
+  }
+
+  void printOptions(List<Option> options) {
+    for (var i = 0; i < options.length; ++i) {
+      stdout.writeln('[$i] - ${options[i].label}');
+    }
+
+    stdout.writeln('Enter a choice\n');
+    stdout.write('>');
+  }
+
+  String collectInput() {
+    return stdin.readLineSync();
+  }
+
+  void clearScreen() {
+    if (Platform.isWindows) {
+      stdout.write('\x1B[2J\x1B[0f');
+    } else {
+      stdout.write('\x1B[2J\x1B[3J\x1B[H');
+    }
+  }
+}
